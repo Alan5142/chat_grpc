9d138ba662f1fc906e1a4da17c6bd97cd122113b
Alan Ramírez Herrera <alan5142@hotmail.com>
2020-11-09 22:20:29 -0600

    First commit

--- chat_server/src/main/java/com/chat/chat_server/ChatService.java
+++ chat_server/src/main/java/com/chat/chat_server/ChatService.java
@@ -1,7 +1,21 @@
 package com.chat.chat_server;
 
+import com.chat.grpc.ChatGrpc;
+import com.chat.grpc.ChatServer;
+import com.chat.grpc.Uuid;
+import io.grpc.stub.StreamObserver;
 import org.lognet.springboot.grpc.GRpcService;
 
 @GRpcService
-public class ChatService  {
+public class ChatService extends ChatGrpc.ChatImplBase {
+
+    @Override
+    public void onNewMessage(Uuid.UUID request, StreamObserver<ChatServer.ChatMessage> responseObserver) {
+        super.onNewMessage(request, responseObserver);
+    }
+
+    @Override
+    public void sendMessage(ChatServer.SendMessageRequest request, StreamObserver<ChatServer.ChatMessage> responseObserver) {
+        super.sendMessage(request, responseObserver);
+    }
 }
